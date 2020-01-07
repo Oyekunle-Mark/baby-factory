8332d72caddf421428e65fff9cc41bf80c0da0aa
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-07 17:57:34 +0100

    Replaced let with const

--- app/factory.ts
+++ app/factory.ts
@@ -5,13 +5,13 @@ import { Adult } from './adult';
 
 export class Factory {
     getPerson(dateOfBirth: Date): IPerson {
-        let dateNow = new Date();
-        let currentMonth = dateNow.getMonth() + 1;
-        let currentDate = dateNow.getDate();
-        let currentYear = dateNow.getFullYear();
+        const dateNow = new Date();
+        const currentMonth = dateNow.getMonth() + 1;
+        const currentDate = dateNow.getDate();
+        const currentYear = dateNow.getFullYear();
 
-        let dateTwoYearsAgo = new Date(currentYear - 2, currentMonth, currentDate);
-        let dateEighteenYearsAgo = new Date(currentYear - 18, currentMonth, currentDate);
+        const dateTwoYearsAgo = new Date(currentYear - 2, currentMonth, currentDate);
+        const dateEighteenYearsAgo = new Date(currentYear - 18, currentMonth, currentDate);
 
         if (dateOfBirth >= dateTwoYearsAgo) {
             return new Infant(dateOfBirth);
