5a7613e72842c946af642c8e76801645d47901a6
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-07 18:00:58 +0100

    Created person using the factory

---
+++ index.ts
@@ -0,0 +1,12 @@
+import { Factory } from './app/factory';
+
+const factory = new Factory();
+
+const person1 = factory.getPerson(new Date(2001, 9, 11));
+person1.printDetails();
+
+const person2 = factory.getPerson(new Date(1990, 10, 1));
+person2.printDetails();
+
+const person3 = factory.getPerson(new Date());
+person3.printDetails();
