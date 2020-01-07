2ac232de1db8f040d44325936efc9deca36fba34
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-07 17:19:32 +0100

    The person abstract base class

--- app/person.ts
+++ app/person.ts
@@ -9,3 +9,21 @@ interface IPerson {
     canSignContract(): boolean;
     printDetails(): void;
 }
+
+export abstract class Person implements IPerson {
+    Category: PersonCategory;
+    private DateOfBirth: Date;
+
+    constructor(dateOfBirth: Date) {
+        this.DateOfBirth = dateOfBirth;
+    }
+    
+    abstract canSignContract(): boolean
+
+    printDetails() {
+        console.log('Person: ');
+        console.log(`Date of Birth: ${this.DateOfBirth.toDateString()}`);
+        console.log(`Category: ${PersonCategory[this.Category]}`);
+        console.log(`Can sign: ${this.canSignContract()}`);
+    }
+}
